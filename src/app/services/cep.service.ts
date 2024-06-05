133223bf5a4cfcc61097729234c62c0a54c09b58
Robérioe <roberioalbuquerque.neto@gmail.com>
2024-06-05 13:20:41 -0300

    feat: Construção da lógica de requisição htttp para a API de BuscaCEP

--- src/app/services/cep.service.ts
+++ src/app/services/cep.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http"
+import { Observable } from "rxjs";
+
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CepService {
 
-  constructor() { }
+    private url = `https://viacep.com.br/ws/`
+
+    constructor(private httpClient: HttpClient) {
+
+    }
+
+    obterCep(cep: string): Observable<any> {
+        const apiUrl = `${this.url}${cep}/json/`
+        return this.httpClient.get<any>(apiUrl)
+    }   
+
 }
