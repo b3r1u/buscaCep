5ffcd9b38fe10b4f6b2c520f24700ab56dc66681
Robérioe <roberioalbuquerque.neto@gmail.com>
2024-06-05 13:28:06 -0300

    feat: Criando formulário para reber o CEP e também adicionado os tratamentos de interação de campos com ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 import { LogoCepComponent } from './components/logo-cep/logo-cep.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LogoCepComponent } from './components/logo-cep/logo-cep.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { CepService } from 'src/app/services/cep.service';
+import { CepResponse } from 'src/app/interface/cep-response';
+
+
 
 @Component({
   selector: 'app-form',
@@ -6,5 +10,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
+
+  logradouro: string = '';
+  bairro: string = '';
+  localidade: string = '';
+  uf: string = ''; 
+  resultadoDisponivel: boolean = false;
+  loading: boolean = false;
+  cepInvalido: boolean = false;
+
+  constructor(private cepService: CepService) {}
+
+  resetaCampos() {
+    this.logradouro = '';
+    this.bairro = '';
+    this.localidade = '';
+    this.uf = '';
+    this.resultadoDisponivel = false;
+    this.cepInvalido = false;
+  }
+
+  buscandoCep(cep: string): void {
+    this.resetaCampos()
+    this.loading = true;
+
+    const cepLimpo = cep.replace(/\D/g, ''); // removendo caracteres não numéricos
+
+    if(cepLimpo.length !== 8) {
+      this.cepInvalido = true;
+      this.loading = false;
+      return;
+    }
+
+    this.cepService.obterCep(cep).subscribe(
+      (data: CepResponse) => {
+        if (this.isValidCepResponse(data)) {
+          this.logradouro = data.logradouro
+          this.bairro = data.bairro
+          this.localidade = data.localidade
+          this.uf = data.uf
+          this.hideLoaderAfterDelay();
+        } else {
+          this.loading = false;
+          this.cepInvalido = true;
+        }
+      },
+      (error) => {
+        console.log('sem resultados')
+        this.loading = false;
+        this.cepInvalido = true;
+      }
+    );
+  }
+
+  isValidCepResponse(data: CepResponse): boolean {
+    return !!(data.logradouro || data.bairro || data.localidade || data.uf);
+  }
+
+  hideLoaderAfterDelay(): void {
+    setTimeout(() => {
+      this.loading = false;
+      this.resultadoDisponivel = true;
+    }, 1500);
+  }
 
 }
